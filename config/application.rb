6a7907cd15ca6af194cac038e4bb11b934c6f4b8
Vlad Ivanovic <vladiim@me.com>
2012-05-30 21:56:25 +1000

    added snapz and pry

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Fitafy
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/lib/modules)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -55,5 +56,13 @@ module Fitafy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+  end
+end
+
+silence_warnings do
+  begin
+    require 'pry'
+    IRB = Pry
+  rescue LoadError
   end
 end
---
+++ lib/modules/snapz_sayz.rb
@@ -0,0 +1,38 @@
+module SnapzSayz
+
+  class Information
+  	def self.title
+  	  "fitafy fly"
+  	end
+
+  	def self.description
+  	  "fitafy helps personal trainers keep track of their client's workouts and stay fly"
+  	end
+  end
+
+  class ExerciseSpeak
+    def self.all_exercises
+      "Exercises up the yoyo!"
+    end
+
+  	def self.creating_new_exercise
+  	  "Creatin' a new exercise? Just give me some deets"
+  	end
+
+    def self.editing_exsisting_exercise
+      "Aint this exercise good enough for ya? I always thought you were a perfectionist!"
+    end
+
+    def self.created_exercise
+      "Boom! New exercise up in this mo fo!"
+    end
+
+    def self.updated_exercise
+      "Man you updated the smack outta that exercise!"
+    end
+
+    def self.deleting_exercise_confirmation
+      "Woah cowboy... you really wanna kill this exercise?"
+    end
+  end
+end
