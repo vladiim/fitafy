9761335ac57bbfb50d9f706bd74d9e300d214a03
Vlad Ivanovic <vladiim@me.com>
2012-06-01 20:25:44 +1000

    Exercise validates_presence_of :name, :description

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,7 +1,7 @@
 class Exercise < ActiveRecord::Base
   attr_accessible :categories, :description, :equipment, :name, :tips
 
-  validates :name, :description, presence: true
+  validates_presence_of :name, :description
 
   scope :alphabetical_order, -> { order :name }
 end
--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -4,6 +4,15 @@ include CreateArelExercise
 describe Exercise do
   subject { Exercise.new }
 
+  def create_valid_exercise
+    Exercise.new(name:        "robert diggs",
+                 description: "rza",
+                 tips:        "shine 7 in the sun",
+                 categories:  "chessboxing",
+                 equipment:   "phaser on stun"
+    )
+  end
+
   describe "#alphabetical_order" do
     before do
       @b = create_exercise "BBBB"
@@ -13,5 +22,15 @@ describe Exercise do
     it "should return an exercise list in alphabetical order" do
       Exercise.alphabetical_order.should eq [@a, @b, @c]
     end
+  end
+
+  describe "#validations" do
+    before { @exercise = create_valid_exercise }
+    context "with name" do
+       it "should be valid" do
+         @exercise.name = nil
+         @exercise.should_not be_valid
+       end
+     end
   end
 end
