6ea3ce3264fefe9d81297b24380da68c64034897
Vlad Ivanovic <vladiim@me.com>
2012-06-15 21:03:00 +1000

    cleaned up workout_spec.rb

--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -20,35 +20,9 @@ describe Workout do
     it "should capitalize the notes" do
       subject.capitalize_notes.should eq "Dealin in my cypher. i revolve around sciences"
     end
-
-    describe "#exercises" do
-      it "should accept a hash of exercise attributes" do
-        subject.exercises.should eq exercises
-      end
-
-      it "should accept an id"
-    end
   end
 
   describe "#validate" do
-
-    describe "exercises" do
-      context "with valid attributes" do
-        it "should be valid" do
-          subject.exercises = CreateWorkoutExercise.valid
-          subject.should be_valid
-        end
-      end
-
-       context "without exercises" do
-         it "should't be valid" do
-           subject.exercises = nil
-           subject.should_not be_valid
-         end
-       end
-    end
-
-    # before { mock(subject).exercises_are_right_type { true } }
 
     context "without name" do
        it "shouldn't be valid" do
@@ -67,38 +41,9 @@ describe Workout do
 
   describe "#all_exercises" do
     it "should call all exercises" do
+      exercises = Object.new
       mock(Exercise).all { exercises }
       subject.all_exercises.should eq exercises
-    end
-  end
-
-  describe "#exercises_are_right_type" do
-
-    context "with an exercise that's the wrong size" do
-      before { mock(subject).exercises { [{ id: 1, sets: 4 }] } }
-      it "should raise a too short error" do
-        lambda { subject.exercises_are_right_type }.
-                 should raise_error,
-                 "Oops, need some more attributes in this exercise!"
-      end
-    end
-
-    context "with an exercise key that's the wrong type" do
-      before { mock(subject).exercises { [{ id: 1, sets: 4, cuban_lynx: [3,4,5] }] } }
-      it "should raise a wrong key error" do
-        lambda { subject.exercises_are_right_type }.
-                 should raise_error,
-                 "One o' these keys is messed up!"
-      end
-    end
-
-    context "with an exercise value that's the wrong type" do
-      before { mock(subject).exercises { [{ id: 1, sets: 4, sets: [3,4,""] }] } }
-      it "should raise a wrong value error" do
-        lambda { subject.exercises_are_right_type }.
-                 should raise_error,
-                 "Wayda second! That value is wrong hombre!"
-      end
     end
   end
 
