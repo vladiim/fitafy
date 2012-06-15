ef8e8dd4d28ea7bcf9f5b36dfbdac2d48c607878
Vlad Ivanovic <vladiim@me.com>
2012-06-15 19:42:08 +1000

    WorkoutExercise validates_presence of workout_id and that it's an Integer

--- app/models/workout_exercise.rb
+++ app/models/workout_exercise.rb
@@ -1,3 +1,23 @@
+require 'active_model'
+
+class IntegerValidator < ActiveModel::Validator
+  def validate record
+  	unless record.is_a? Integer
+      record.errors[:base] << "Not an Integer"
+  	end
+  end
+end
+
 class WorkoutExercise
+  include ActiveModel::Validations
+  include ActiveModel::Validations
   attr_accessor :workout_id, :exercise_id, :sets, :rep_weight, :rep_time
+
+  def initialize attrs={}
+  	attrs.each { |key, value| self.send"#{key}=", value }
+  end
+
+  validates_presence_of :workout_id
+
+  validates_with IntegerValidator, fields: [:workout_id]
 end
