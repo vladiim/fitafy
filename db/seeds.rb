e44f773f40501baecfceab005c747b824c98fb06
Vlad Ivanovic <vladiim@me.com>
2012-06-27 20:47:07 +1000

    revised seed file for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,13 @@ Dir[File.dirname(__FILE__) + '/seed_files/**/*.rb'].each { |file| require file }
 
 # First create all the Exercises
 create_back
-create_biceps
-create_chest
-create_core
-create_forearms
-create_legs
-create_shoulders
-create_triceps
+# create_biceps
+# create_chest
+# create_core
+# create_forearms
+# create_legs
+# create_shoulders
+# create_triceps
 
 # Then create workouts which rely on Exercises to exsist
-create_workouts
+# create_workouts
