f74ff641eb46c23405c8811bcf4c61819acbb490
Vlad Ivanovic <vladiim@me.com>
2013-01-08 18:35:08 +1100

    Fixed heroku issue

--- db/migrate/20130108062514_add_gym_and_bio_to_users.rb
+++ db/migrate/20130108062514_add_gym_and_bio_to_users.rb
@@ -1,13 +1,6 @@
 class AddGymAndBioToUsers < ActiveRecord::Migration
-  def up
-  	remove_column :users, :gym
-  	remove_column :users, :bio
+  def change
     add_column :users, :gym, :string, default: 'No gym listed'
     add_column :users, :bio, :text, default: 'No bio given.'
-  end
-
-  def down
-  	remove_column :users, :gym
-  	remove_column :users, :bio
   end
 end
