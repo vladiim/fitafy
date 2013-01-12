23e7428225e2b0d3ee9b4551134c8ab34a491d3c
Vlad Ivanovic <vladiim@me.com>
2013-01-12 12:43:03 +1100

    Refactored ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,27 +9,45 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, alert: SnapzSayz::Information.unauthorized
   end
 
-  def redirect_back params=nil
-    if current_user && session[:redirect_to]
-      redirect_to session[:redirect_to]
-      session[:redirect_to] = nil
-    elsif params != nil
-      redirect_to params
+  def redirect_back(params=nil)
+    if redirect_to_info?
+      send_to_redirect_to
     else
-      redirect_to root_path
+      params_arent_empty ? redirect_to(params) : redirect_to(root_path)
     end
   end
 
   def current_user
     return @current_user if defined?(@current_user)
-    if session[:facebook_user_id]
-      @current_user = User.find(session[:facebook_user_id])
-    else
-      @current_user = current_user_session && current_user_session.user
-    end
+    is_facebook_session ? find_user_via_facebook : find_user_via_user_session
   end
 
   private
+
+    def params_arent_empty
+      params != nil
+    end
+
+    def send_to_redirect_to
+      redirect_to session[:redirect_to]
+      session[:redirect_to] = nil
+    end
+
+    def redirect_to_info?
+      current_user && session[:redirect_to]
+    end
+
+    def find_user_via_user_session
+      @current_user = current_user_session && current_user_session.user
+    end
+
+    def is_facebook_session
+      session[:facebook_user_id]
+    end
+
+    def find_user_via_facebook
+      @current_user = User.find(session[:facebook_user_id])
+    end
 
     def current_user_session
       return @current_user_session if defined?(@current_user_session)
