b51bb50217f588c0aaa0c11a78f21da36ef88da5
Vlad Ivanovic <vladiim@me.com>
2013-03-31 14:49:17 +1100

    Revised tests to work together

--- spec/integration/users/new/email_sign_up_integration_spec.rb
+++ spec/integration/users/new/email_sign_up_integration_spec.rb
@@ -9,18 +9,18 @@ describe 'Visitor goes to homepage' do
   context 'signs up successfully' do
     before { sign_up }
 
-    # it 'creates a new user with the email' do
-    #   user_email.should eq email
-    # end
+    it 'sends the user a welcome email' do
+      last_email.to.should eq [email]
+      last_email.subject.should eq CopyGenerator::EmailCopy.signup_subject
+    end
 
-    # it 'gives me a success message' do
-    #   page.should have_content CopyGenerator::UserCopy.first_sign_up
-    # end
+    it 'creates a new user with the email' do
+      user_email.should eq email
+    end
 
-    # it 'sends the user a welcome email' do
-    #   last_email.to.should eq [email]
-    #   last_email.subject.should eq CopyGenerator::EmailCopy.signup_subject
-    # end
+    it 'gives me a success message' do
+      page.should have_content CopyGenerator::UserCopy.first_sign_up
+    end
   end
 
   context 'signs up twice' do
@@ -30,15 +30,6 @@ describe 'Visitor goes to homepage' do
       page.should have_content CopyGenerator::UserCopy.error_first_sign_up
       page.should have_content 'has already been taken'
     end
-  end
-
-  context 'bad email' do
-    # let(:bad_email) { 'test@email' }
-
-    # before do
-    #   fill_in 'user_record_email', with: bad_email
-    #   click_button 'SIGN UP'
-    # end
   end
 end
 
--- spec/services/user/manager_spec.rb
+++ spec/services/user/manager_spec.rb
@@ -13,23 +13,19 @@ describe User::Manager do
     end
 
     describe '#add_to_database' do
+      class SignupMailer; end
+
       let(:result) { user.add_to_database }
 
-      before { mock(record).save { true } }
+      before do
+        mock(record).save { true }
+        mock(SignupMailer).welcome_email(anything) { SignupMailer }
+        mock(SignupMailer).deliver { 'MAIL DELIVERED' }
+      end
 
       it 'delivers the user an email' do
       	result.should eq 'MAIL DELIVERED'
       end
     end
-  end
-end
-
-class SignupMailer
-  def self.welcome_email(user)
-  	self
-  end
-
-  def self.deliver
-  	'MAIL DELIVERED'
   end
 end
