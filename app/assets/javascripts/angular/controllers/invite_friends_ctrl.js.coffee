28d663bd8bc544745d202e6c78ecef617e944fc3
Vlad Ivanovic <vladiim@me.com>
2013-04-10 18:41:31 +1000

    Can add and remove friends from invite list

--- app/assets/javascripts/angular/controllers/invite_friends_ctrl.js.coffee
+++ app/assets/javascripts/angular/controllers/invite_friends_ctrl.js.coffee
@@ -3,3 +3,6 @@
 
   $scope.addFriend = (friend) ->
     $scope.friends.push(friend)
+
+  $scope.removeFriend = (index) ->
+    $scope.friends.splice(index, 1)
--- spec/integration/invites/add_friend_to_invite_integration_spec.rb
+++ spec/integration/invites/add_friend_to_invite_integration_spec.rb
@@ -8,12 +8,25 @@ describe "Add friend to invite", js: true do
   	  within('.invited-friends-list') do
   	  	fill_in 'friends_name', with: 'NAME'
   	  	fill_in 'friends_email', with: 'friend@email.com'
-  	  	click_button 'ADD INVITE'
+  	  	click_button 'ADD FRIEND'
   	  end
   	end
 
-	it 'adds the friend to the list' do
-		page.should have_selector('.invited-friend b.name', text: 'NAME')
-	end
+	  it 'adds the friend to the list' do
+		  page.should have_friend
+	  end
+
+    context 'click [x]' do
+      it 'removes the friend from the list' do
+        within('.invited-friends-list') do
+          click_link 'X'
+          page.should_not have_friend
+        end
+      end
+    end
   end
+end
+
+def have_friend
+  have_selector('.invited-friend b.name', text: 'NAME')
 end
